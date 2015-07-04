d643800d3292a1dc3db16daedcb45543cbb2e180
Laurence Nicol <laurence.nicol@veitchlister.com.au>
2015-07-04 17:11:56 +1000

    Added cox input calculation

--- ParcMeRb/go.rb
+++ ParcMeRb/go.rb
@@ -2,9 +2,11 @@ require_relative 'data_reader'
 require_relative 'hazard_data'
 
 directory = File.join(File.dirname(__FILE__),'..')
-csv_file = '/Users/timveitch/Documents/Learning/ParcMe/Parking_Events_2014.csv'
+csv_file = '/Users/LaurenceNicol/Work/GovHack2015/ParcMe/Parking_Events_2014.csv'
 # street_name, between_street_1, between_street_2, side_code, bay_id, sign, date, start_time, end_time
-events = ParcMe::DataReader.read_parking_events_from_csv(csv_file, 50000)
+events = ParcMe::DataReader.read_parking_events_from_csv(csv_file, 50000) { |record|
+  record.fetch("SignPlateId") == 6.to_s
+}
 
 class Hash
   def map_values
@@ -12,8 +14,8 @@ class Hash
   end
 end
 
-write_hazard_data(events,120,directory)
-
+write_hazard_data(events,120, 1, directory)
+write_cox_data(events, 120, 1, directory)
 # by_day = by_day_segment_direction_and_bay(events)
 
 
--- ParcMeRb/hazard_data.rb
+++ ParcMeRb/hazard_data.rb
@@ -1,9 +1,9 @@
-def hazard_data(events,max_mins)
+def hazard_data(events,max_mins, interval_size)
   total_events = events.size
   still_going = events.dup
 
-  (1..max_mins).map { |mins|
-    secs = mins * 60
+  (1..max_mins/interval_size).map { |mins|
+    secs = mins * 60*interval_size
     start_size = still_going.size
     fails = still_going.select { |event|
       event.duration <= secs
@@ -13,14 +13,45 @@ def hazard_data(events,max_mins)
       event.duration > secs
     }
 
-    [mins, fails, start_size]
+    [secs, fails, start_size, fails.to_f/start_size.to_f]
   }
 end
 
-def write_hazard_data(events,max_mins,dir)
+def write_hazard_data(events,max_mins, interval_size, dir)
   File.open(File.join(dir,'hazard_data.csv'),'w') { |file|
-    file.puts 'mins, cumulative_fails, total_events'
-    hazard_data(events,max_mins).each { |data|
+    file.puts 'mins, cumulative_fails, total_events, hazard'
+    hazard_data(events,max_mins, interval_size).each { |data|
+      file.puts data.join(',')
+    }
+  }
+end
+
+def cox_input_data(events,max_mins, interval_size)
+  total_events = events.size
+  events_set = events.dup.zip(1..total_events)
+  data = []
+  (1..max_mins/interval_size).map { |mins|
+    new_events_set = []
+    secs = mins * 60*interval_size
+
+    events_set.each { |event|
+      if event.first.duration <= secs
+        data << [event.last, secs, 1]
+      else
+        data << [event.last, secs, 0]
+        new_events_set << event
+      end
+    }
+    events_set = new_events_set
+  }
+
+  data
+end
+
+def write_cox_data(events,max_mins, interval_size,dir)
+  File.open(File.join(dir,'cox_data.csv'),'w') { |file|
+    file.puts 'ID, time, status'
+    cox_input_data(events,max_mins, interval_size).each { |data|
       file.puts data.join(',')
     }
   }
