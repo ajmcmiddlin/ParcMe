b539d20074c41adcfc1f5bb9f36f9b3063130560
Laurence Nicol <laurence.nicol@veitchlister.com.au>
2015-07-05 12:25:00 +1000

    cdc plotter

--- ParcMeRb/go.rb
+++ ParcMeRb/go.rb
@@ -5,7 +5,7 @@ directory = File.join(File.dirname(__FILE__),'..')
 csv_file = '/Users/LaurenceNicol/Work/GovHack2015/ParcMe/Parking_Events_2014.csv'
 # street_name, between_street_1, between_street_2, side_code, bay_id, sign, date, start_time, end_time
 events = ParcMe::DataReader.read_parking_events_from_csv(csv_file, 100000) { |record|
-  record.fetch("SignPlateId") == 6.to_s
+  record.fetch("Sign") == 6.to_s
 }
 
 class Hash
@@ -17,9 +17,7 @@ end
 # hazard_hash = get_hazard_hash(events, 120, 1)
 # puts calc_hazard_for_duration(hazard_hash, 3000)
 cdf_hash = get_cdf_hash(events,120,1)
-puts calc_probability_for_duration_change(cdf_hash, 45, 5)
-cdf_hash = get_cdf_hash(events,120,15)
-puts calc_probability_for_duration_change(cdf_hash, 45, 5)
+puts cdf_hash.inspect
 write_density_data(cdf_hash, directory)
 write_hazard_data(events,120, 1, directory)
 # write_cox_data(events, 120, 1, directory)
